374c7f04a3f0b18b83148194e60503b7fa930751
jacrestudios <jacrestudios@gmail.com>
2019-06-13 11:03:28 -0600

    Create DownpourNodes.js

---
+++ DownpourNodes.js
@@ -0,0 +1,18 @@
+var nodes = [
+{id: "root", parent: ""},
+{id: "Sleepy Snake", parent: "root", type: "location"},
+{id: "Sleepy Snake Inn", parent: "Sleepy Snake", type: "location"}
+{id: "Fangs of the Snake", parent: "root", type: "location"},
+{id: "Belly of the Snake", parent: "root", type: "location"},
+{id: "Snake Bottom", parent: "root", type: "location"},
+{id: "Floodwaters", parent: "root", type: "location"},
+{id: "Oxbow Port", parent: "root", type: "location"},
+{id: "Oxbow Farms", parent: "root", type: "location"},
+{id: "Swampwater", parent: "root", type: "location"},
+{id: "Boggart Bayou", parent: "root", type: "location"},
+{id: "Tribal Tributary", parent: "root", type: "location"},
+{id: "Bogataw Village", parent: "root", type: "location"},
+{id: "The Boss's Pad", parent: "root", type: "location"},
+{id: "Gnome Grotto", parent "root", type: location"},
+{id: "Chaspayu's Chamber", parent: "root", type: "location"},
+]
